1938894403af33b814d62d45c009f2a41cd4f70d
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-26 13:05:23 +0300

    added agent init

---
+++ Models/agent.py
@@ -0,0 +1,10 @@
+class Agent:
+    def __init__(self, id=None, code_name='', real_name='', location='', status='', missions_completed=0):
+        self.id = id
+        self.code_name = code_name
+        self.real_name = real_name
+        self.location = location
+        self.status = status
+        self.missions_completed = missions_completed
+
+
